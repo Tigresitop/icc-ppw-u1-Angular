d5b0f3349a2dba1cc0eb559b70510ffe55e05e95
Tigre <cuji309@gmail.com>
2026-05-03 18:23:31 -0500

    feat: usar control flow moderno en profile page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,18 @@
 import { Routes } from '@angular/router';
 import { HomePage } from './features/home/pages/home-page';
+import { ProfilePage } from './features/profiles/pages/profile-page';
 
 export const routes: Routes = [
     {
-        path: '',
-        component: HomePage,
+    path: '',
+    component: HomePage,
     },
     {
-        path: '**',
-        redirectTo: '',
-    }
+    path: 'profile',
+    component: ProfilePage,
+    },
+    {
+    path: '**',
+    redirectTo: '',
+    },
 ];
-
