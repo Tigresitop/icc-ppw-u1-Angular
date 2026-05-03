8204f68e82476eee0e250c83ce21e4fdff2173b6
Tigre <cuji309@gmail.com>
2026-05-03 16:33:19 -0500

    configuracion router con ruta raiz y wildcard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomePage } from './features/home/pages/home-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomePage,
+    },
+    {
+        path: '**',
+        redirectTo: '',
+    }
+];
+
--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('ppw-angular-21');
+  title = 'ppw-angular-21';
 }
